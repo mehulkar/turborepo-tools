04726535a05895284aec3da82eda7bcc52479182
Mehul Kar <github@mehulkar.com>
2024-05-21 12:02:31 -0500

    run biome on bin/

--- bin/get-deps.mjs
+++ bin/get-deps.mjs
@@ -4,43 +4,43 @@ import { main } from "../src/get-deps.mjs";
 
 import { parseArgs } from "node:util";
 const { values: flags } = parseArgs({
-  strict: true,
-  options: {
-    // The path to your repo. In most cases, just do `-d .` when you're already in the repo dir.
-    directory: {
-      type: "string",
-      multiple: false,
-      short: "d",
-      default: ".",
-    },
+	strict: true,
+	options: {
+		// The path to your repo. In most cases, just do `-d .` when you're already in the repo dir.
+		directory: {
+			type: "string",
+			multiple: false,
+			short: "d",
+			default: ".",
+		},
 
-    package: {
-      type: "string",
-      multiple: false,
-      short: "p",
-    },
+		package: {
+			type: "string",
+			multiple: false,
+			short: "p",
+		},
 
-    recursive: {
-      type: "boolean",
-      multiple: false,
-      short: "r",
-      default: false,
-    },
-  },
+		recursive: {
+			type: "boolean",
+			multiple: false,
+			short: "r",
+			default: false,
+		},
+	},
 });
 
 console.log("flags", flags);
 
 if (!flags.directory) {
-  throw new Error("--directory is required");
+	throw new Error("--directory is required");
 }
 
 if (!flags.package) {
-  throw new Error("--package is required");
+	throw new Error("--package is required");
 }
 
 const recursive = flags.recursive ?? false;
 
 main({ directory: flags.directory, pkg: flags.package, recursive })
-  .then(console.log)
-  .catch(console.error);
+	.then(console.log)
+	.catch(console.error);
--- bin/self-import.mjs
+++ bin/self-import.mjs
@@ -3,32 +3,32 @@
 import { main } from "../src/self-import.mjs";
 
 const { values: flags } = parseArgs({
-  strict: true,
-  options: {
-    "dry-run": {
-      type: "boolean",
-      multiple: false,
-      default: false,
-    },
-    // The path to your repo. In most cases, just do `-d .` when you're already in the repo dir.
-    directory: {
-      type: "string",
-      multiple: false,
-      short: "d",
-      default: ".",
-    },
-    limit: {
-      type: "string",
-      multiple: false,
-      short: "l",
-      default: "10",
-    },
-    only: {
-      type: "string",
-      multiple: true,
-      default: [],
-    },
-  },
+	strict: true,
+	options: {
+		"dry-run": {
+			type: "boolean",
+			multiple: false,
+			default: false,
+		},
+		// The path to your repo. In most cases, just do `-d .` when you're already in the repo dir.
+		directory: {
+			type: "string",
+			multiple: false,
+			short: "d",
+			default: ".",
+		},
+		limit: {
+			type: "string",
+			multiple: false,
+			short: "l",
+			default: "10",
+		},
+		only: {
+			type: "string",
+			multiple: true,
+			default: [],
+		},
+	},
 });
 
 console.log(flags);
