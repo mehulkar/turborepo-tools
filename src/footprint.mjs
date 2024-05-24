12690d4412f2efdb59d5d2d89b1a6c66d2b1f1fe
Mehul Kar <github@mehulkar.com>
2024-05-24 00:53:24 -0500

    Show top 5 importers of a package

--- src/footprint.mjs
+++ src/footprint.mjs
@@ -1,35 +1,54 @@
 import { main as getDependents } from "./get-deps.mjs";
-import { getImportsInPackage } from "./utils/get-imports-in-package.mjs";
+import { getImportsInPackage } from "./utils/get-all-imports.mjs";
 
 export async function main({ dir, pkg }) {
-	const ancestors = await getDependents({
-		directory: dir,
-		pkg,
-		recursive: false,
-	});
+	console.log(`${pkg}:`);
+	await logUsers({ dir, pkg });
+	await logRecursiveUsers({ dir, pkg });
+	await logImporters({ dir, pkg });
+
+	// TODO: which files are imported the most
+}
+
+async function logRecursiveUsers({ dir, pkg }) {
 	const ancestorTree = await getDependents({
 		directory: dir,
 		pkg,
 		recursive: true,
 	});
+	// TODO: sorted by their size
+	console.log(`- ${ancestorTree.length} have it in their tree`);
+}
 
-	console.log(`${pkg}:`);
+async function logUsers({ dir, pkg }) {
+	const ancestors = await getDependents({
+		directory: dir,
+		pkg,
+		recursive: false,
+	});
 	// TODO: sorted by their size
 	console.log(`- ${ancestors.length} packages depend on this`);
+}
 
-	console.log(`- ${ancestorTree.length} have it in their tree`);
-
+async function logImporters({ dir, pkg }) {
 	// This is a map for this package
 	const importsMap = await getImportsInPackage(dir, pkg);
 
 	let totalFiles = 0;
-	for (const [_pkg, files] of importsMap.entries()) {
+	const importers = [];
+	for (const [pkg, files] of importsMap.entries()) {
 		totalFiles += files.length;
+		importers.push({ pkg, files }); // TODO: Can we just use the map.entries() to do this?
 	}
 
-	console.log(
-		`- imported by ${totalFiles} files across ${importsMap.size} packages`,
-	);
+	importers.sort((a, b) => b.files.length - a.files.length);
+	const top5 = importers.slice(0, 5);
 
-	// TODO: which files are imported the most
+	console.log(
+		`- Imported by ${totalFiles} files across ${importsMap.size} packages`
+	);
+	console.log(
+		"- Top 5 importers (by number of files):",
+		top5.map((x) => `${x.pkg}: ${x.files.length} files`)
+	);
 }
